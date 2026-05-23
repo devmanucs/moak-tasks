3c6042bb4f2783284bf2a8b9ac8383691c7485c5
Manuella <manuhcsantos@gmail.com>
2026-05-23 16:35:22 -0300

    feat: adiciona suporte a temas e configurações de estilo com Tailwind CSS

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,28 @@
+import { ThemeProvider } from "@react-navigation/native";
+import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
+import { NAV_THEME } from "../lib/theme";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme() ?? "light";
+
   return (
     <SafeAreaProvider>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="index" />
-      </Stack>
-      <StatusBar style="auto" />
+      <ThemeProvider value={NAV_THEME[colorScheme]}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="index" />
+        </Stack>
+        <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+        <PortalHost />
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
